d1f7d9c6ed53851ff425dd976fc081e01ad7f451
JerryHuang-lab <798457734@qq.com>
2021-05-09 00:58:07 +0800

    [add] 搭建工程基础目录，区分frame层和caiss服务层

---
+++ caiss-start/src/main/java/com/mojito/caiss/server/CaissServerApplication.java
@@ -0,0 +1,14 @@
+package com.mojito.caiss.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaissServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaissServerApplication.class, args);
+	}
+
+}
